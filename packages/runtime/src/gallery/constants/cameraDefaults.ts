7f5d740fe23d05e0136b39920990cc8db2805270
goblinsami <goblinsami@gmail.com>
2026-05-31 20:06:11 +0200

    refacto

--- packages/runtime/src/gallery/constants/cameraDefaults.ts
+++ packages/runtime/src/gallery/constants/cameraDefaults.ts
@@ -1,8 +1,1 @@
-export const CAMERA_DEFAULTS = {
-  near: 0.1,
-  far: 400,
-  fov: 52,
-  height: 1.7,
-  movementSmoothing: 0.14,
-} as const;
-
+export * from "../../../../../src/gallery/constants/cameraDefaults.ts";
--- packages/runtime/src/gallery/utils/clamp.ts
+++ packages/runtime/src/gallery/utils/clamp.ts
@@ -1,3 +1,1 @@
-export const clamp = (value: number, min: number, max: number): number =>
-  Math.min(max, Math.max(min, value));
-
+export * from "../../../../../src/gallery/utils/clamp.ts";
